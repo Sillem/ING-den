5ff28ef80fd7f90fa798afef0ffb0015806d0dd3
Karol Wojciechowski <karolw2208@gmail.com>
2024-03-12 16:23:23 +0100

    updated evaluation methods

--- ING_chall.py
+++ ING_chall.py
@@ -246,7 +246,7 @@ print("F1-score:", f1_logistic)
 # GAM Model
 print("\nGeneralized Additive Model (GAM) Model:")
 gam_model = LogisticGAM(s(0) + s(1) + s(2) +s(3) + s(4) + s(5)+ s(6) + s(7) + s(8)+s(9) + s(10) + s(11)+s(12) + s(13) + s(14)+s(15) + s(16) + s(17)+s(18) + s(19) + s(20)+s(21) + s(22) + s(23)+s(24) + s(25) + s(26)+s(27) + s(28) + s(29)+s(30) + s(31) + s(32) +s(33)).fit(X, y)
-
+gam = LogisticGam(reduce(lambda x, y: s(x) + s(y), [i for i in range(34)]).fit(X,y)
 # Plot partial dependence for each feature
 #for i, term in enumerate(gam_model.terms):
     #if term.isintercept:
